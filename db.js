3384ef9b493b7f340afc545e1a937ad12c785d0f
jet-inigo <jetgeronimo@gmail.com>
2026-01-15 03:20:14 -0600

    Revert: unset "type": "module" in package.json

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
--- models/matches.js
+++ models/matches.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const matchSchema = new mongoose.Schema({
   Winners: [String],
