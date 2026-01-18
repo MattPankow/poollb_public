ccfef408a30716f6fbc9b88bb435f755a7d6bb09
jet-inigo <jetgeronimo@gmail.com>
2026-01-18 00:39:19 -0600

    Minor formatting changes in delete.js

--- routes/delete.js
+++ routes/delete.js
@@ -1,20 +1,25 @@
 import express from "express";
 import Match from "../models/matches.js";
 import Player from "../models/players.js";
+
 const router = express.Router();
+
 // DELETE route to remove a game by its _id
 router.delete("/:gameId", async (req, res) => {
   const gameId = req.params.gameId;
-  console.log("attempt");
+
   try {
     // Find the game by its _id and remove it
     const removedGame = await Match.findByIdAndRemove(gameId);
+
     if (!removedGame) {
       // Game not found
       return res.status(404).send("Game not found");
     }
+
     // Calculate the reversed ratingChange for winners and losers
     const reversedRatingChange = -removedGame.ratingChange;
+
     // Update the player ratings
     // Function to update player rating
     const updatePlayerRating = async (playerName, ratingChange) => {
@@ -34,14 +39,17 @@ router.delete("/:gameId", async (req, res) => {
         console.error("Error updating player rating:", error);
       }
     };
+
     // Update ratings for winners (negative change)
     for (const winner of removedGame.Winners) {
       await updatePlayerRating(winner, reversedRatingChange);
     }
+
     // Update ratings for losers (positive change)
     for (const loser of removedGame.Losers) {
       await updatePlayerRating(loser, removedGame.ratingChange);
     }
+
     // Send a success response
     res.status(200).send("Game removed successfully");
   } catch (error) {
@@ -49,4 +57,6 @@ router.delete("/:gameId", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
 export default router;
+
