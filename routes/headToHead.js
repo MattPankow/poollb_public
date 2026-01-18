c660159bbb60ba36fc50bd3d9e6e2b62b9606d1f
jet-inigo <jetgeronimo@gmail.com>
2026-01-18 00:39:14 -0600

    Minor formatting changes in headToHead.js

--- routes/headToHead.js
+++ routes/headToHead.js
@@ -1,8 +1,10 @@
 import express from "express";
 import Player from "../models/players.js";
 import Match from "../models/matches.js";
+
 const router = express.Router();
-router.get("/", (req, res) => {
+
+router.get("/", (_, res) => {
   // Retrieve player data from the MongoDB collection
   Player.find()
     .sort({ name: 1 })
@@ -19,19 +21,20 @@ router.get("/", (req, res) => {
       res.status(500).send("Internal Server Error");
     });
 });
+
 router.post("/", async (req, res) => {
   console.log("POSTING HEAD TO HEAD");
   const player1 = req.body.player1;
   const player2 = req.body.player2;
-  console.log(player1);
+
   try {
     // Query the database to get head-to-head information
     const headToHeadRecord = await getHeadToHeadRecord(player1, player2);
     const headToHeadGames = await getHeadToHeadGames(player1, player2);
-    console.log(headToHeadRecord);
-    console.log(headToHeadGames);
+
     // Retrieve player data from the MongoDB collection
     const players = await Player.find().sort({ name: 1 });
+
     // Render the headToHead.ejs template and pass the player data along with other variables
     res.render("headToHead", { players, headToHeadRecord, headToHeadGames });
   } catch (error) {
@@ -39,6 +42,7 @@ router.post("/", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
 const getHeadToHeadRecord = async (player1, player2) => {
   // Implement logic to retrieve the all-time record between player1 and player2
   // You may query your database to get the head-to-head record
@@ -55,13 +59,15 @@ const getHeadToHeadRecord = async (player1, player2) => {
       },
     ],
   });
-  console.log(matches);
+
   const winsPlayer1 = matches.filter((match) =>
     match.Winners.includes(player1),
   ).length;
   const winsPlayer2 = matches.length - winsPlayer1;
+
   return `${player1}: ${winsPlayer1} wins, ${player2}: ${winsPlayer2} wins`;
 };
+
 const getHeadToHeadGames = async (player1, player2) => {
   // Implement logic to retrieve the list of head-to-head games between player1 and player2
   // You may query your database to get the head-to-head games
@@ -69,9 +75,12 @@ const getHeadToHeadGames = async (player1, player2) => {
   const matches = await Match.find({
     $or: [{ Winners: [player1, player2] }, { Winners: [player2, player1] }],
   });
+
   return matches.map(
     (match) =>
       `${match.Winners.join(" and ")} vs ${match.Losers.join(" and ")}`,
   );
 };
+
 export default router;
+
