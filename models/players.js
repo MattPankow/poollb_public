b3b78bd83a830d1fbc8f66aa069634ba547eb058
jet-inigo <jetgeronimo@gmail.com>
2026-01-17 22:46:40 -0600

    Add newlines to players.js

--- models/players.js
+++ models/players.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+
 const playerSchema = new mongoose.Schema({
   name: String,
   rating: Number,
 });
+
 const Player = mongoose.model("Player", playerSchema);
+
 export default Player;
+
