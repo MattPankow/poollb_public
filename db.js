4afdfd7f2544e138e640e845dbffe1986cba13a9
jet-inigo <jetgeronimo@gmail.com>
2026-01-17 22:44:48 -0600

    Add newlines to db.js

--- db.js
+++ db.js
@@ -3,12 +3,14 @@ import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
+
 const {
   MONGO_HOSTNAME,
   MONGO_INITDB_ROOT_USERNAME,
   MONGO_INITDB_ROOT_PASSWORD,
   MONGO_INITDB_DATABASE,
 } = process.env;
+
 async function readPlayerNamesFromFile() {
   return new Promise((resolve, reject) => {
     const filePath = path.join(import.meta.dirname, "players.csv");
@@ -18,15 +20,19 @@ async function readPlayerNamesFromFile() {
         reject(error);
         return;
       }
+
       // Split the data by newline characters to get an array of names
       const namesArray = data.split(/\r?\n/).map((name) => name.trim());
+
       resolve(namesArray);
     });
   });
 }
+
 const initializeDatabase = async () => {
   try {
     console.log("Connecting to MongoDB");
+
     const url = new URL(
       `mongodb://${MONGO_HOSTNAME}:27017/${MONGO_INITDB_DATABASE}`,
     );
@@ -38,20 +44,24 @@ const initializeDatabase = async () => {
       authSource: "admin",
     }).toString();
     const mongoUri = url.toString();
+
     await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     const db = mongoose.connection.db;
+
     const collection = db.collection("players");
     await collection.createIndex({ name: 1 }, { unique: true });
     const playerNames = await readPlayerNamesFromFile();
     const playersData = playerNames.map((name) => ({ name, rating: 1000 }));
+
     // Use bulk insertion for efficiency
     const bulk = collection.initializeUnorderedBulkOp();
     playersData.forEach((player) => {
       bulk.insert(player);
     });
+
     // Execute the bulk insert operation
     try {
       await bulk.execute();
@@ -68,4 +78,6 @@ const initializeDatabase = async () => {
     console.error("Error connecting to MongoDB:", error);
   }
 };
+
 export default initializeDatabase;
+
